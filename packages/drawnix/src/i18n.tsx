6e52b6e6aace0c8bdd13af044b8b0b20a2a7b522
Ray <450419074@qq.com>
2025-09-27 22:25:32 +0800

    删除了很多文件

--- packages/drawnix/src/i18n.tsx
+++
@@ -1,2 +0,0 @@
-export { I18nProvider, useI18n, i18nInsidePlaitHook } from './i18n/index';
-export type { Language, Translations, I18nContextType } from './i18n/types';
--- packages/drawnix/src/i18n/translations/index.ts
+++
@@ -1,6 +0,0 @@
-import zhTranslations from './zh';
-import enTranslations from './en';
-import ruTranslations from './ru';
-import arTranslations from './ar';
-
-export { zhTranslations, enTranslations, ruTranslations,arTranslations };
